8c84c3701351fa3437ad961ece3ca16445688e09
Keith Bloom <keith.bloom@gmail.com>
2013-01-25 09:32:10 +0000

    Updated shortcuts

--- environment.ps1
+++ environment.ps1
@@ -15,10 +15,10 @@ function Start-Project {
 	cd $work\'LocalBuild'
 }
 
-function services { Start-Project -hash @{Path='APMG.Services.WCF';Solution='.\APMG.Services.WCF.sln'}}
+function services { Start-Project -hash @{Path='APMG.Services';Solution='.\APMG.Services.WCF.sln'}}
 function portal { Start-Project -hash @{Path='APMG.Portal';Solution='.\APMG.Portal.sln'}}
 function admin { Start-Project -hash @{Path='APMG.Admin';Solution='.\APMG.Admin.sln'}}
-function booking { Start-Project -hash @{Path='APMG.Services.WCF\APMG.Services.WCF.Booking';Solution='.\APMG.Services.WCF.Booking.sln'}}
+function booking { Start-Project -hash @{Path='APMG.Services\APMG.Services.WCF.Booking';Solution='.\APMG.Services.WCF.Booking.sln'}}
 
 function  work { Set-Location-Dev $work }
 function  dev { Set-Location-Dev $dev }
