389160493e589d1488cb7d09f5f3b16b544f1c77
Keith Bloom <keith.bloom@gmail.com>
2016-09-25 17:07:46 +0100

    Adds script to install dependcies and to install profile

---
+++ bootstrap.ps1
@@ -0,0 +1,9 @@
+& git submodule update --init --recursive
+$profileDir = Split-Path -parent $profile
+$poshGitDir = Join-Path $profileDir "posh-git"
+if (![System.IO.Directory]::Exists($profileDir)) {[System.IO.Directory]::CreateDirectory($profileDir)}
+if (![System.IO.Directory]::Exists($profileDir)) {[System.IO.Directory]::CreateDirectory($poshGitDir)}
+Copy-Item -Path ./*.ps1 -Destination $profileDir -Force -Exclude "bootstrap.ps1"
+Copy-Item -Path ./posh-git/** -Destination $poshGitDir -Force -Include **
+Remove-Variable profileDir
+Remove-Variable poshGitDir
---
+++ deps.ps1
@@ -0,0 +1,53 @@
+# Update Help for Modules
+Update-Help -Force
+
+### Package Providers
+# Get-PackageProvider NuGet -Force
+# Chocolatey Provider is not ready yet. Use normal Chocolatey
+#Get-PackageProvider Chocolatey -Force
+#Set-PackageSource -Name chocolatey -Trusted
+
+### Chocolatey
+if ((which cinst) -eq $null) {
+    iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+    Refresh-Environment
+    choco feature enable -n=allowGlobalConfirmation
+}
+
+# system and cli
+#cinst curl #`curl` comes with GH4W
+cinst nuget.commandline
+cinst webpi
+cinst wget
+cinst wput
+
+# browsers
+cinst GoogleChrome
+cinst Firefox
+
+# dev tools and frameworks
+cinst atom
+cinst GitHubForWindows
+cinst Fiddler4
+cinst node
+cinst npm
+cinst vim
+cinst winmerge
+
+
+### Visual Studio Plugins
+if (which Install-VSExtension) {
+    ### Visual Studio 2015
+    # VsVim
+    Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/59ca71b3-a4a3-46ca-8fe1-0e90e3f79329/file/6390/57/VsVim.vsix
+    # Productivity Power Tools 2015
+    Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/34ebc6a2-2777-421d-8914-e29c1dfa7f5d/file/169971/1/ProPowerTools.vsix
+
+    ### Visual Studio 2013
+    # VsVim
+    # Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/59ca71b3-a4a3-46ca-8fe1-0e90e3f79329/file/6390/57/VsVim.vsix
+    # Productivity Power Tools 2013
+    # Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/dbcb8670-889e-4a54-a226-a48a15e4cace/file/117115/4/ProPowerTools.vsix
+    # Web Essentials 2013
+    # Install-VSExtension https://visualstudiogallery.msdn.microsoft.com/56633663-6799-41d7-9df7-0f2a504ca361/file/105627/47/WebEssentials2013.vsix
+}
